b3cdc3ac44f5b9333b50816795b576affb18f51c
MarceloSilveira <marcelosilverag@hotmail.com>
2024-02-21 19:16:32 -0300

    first commit

---
+++ src/CalculoMedia.java
@@ -0,0 +1,14 @@
+public class CalculoMedia {
+
+    public  static void main (String args []) {
+
+        int nota1 = 70;
+        int nota2 = 95;
+        int nota3 = 100;
+        int nota4 = 60;
+        int media = (nota1 + nota2 + nota3 + nota4) / 4 ;
+
+        System.out.println("Media final:" + media);
+    }
+}
+
